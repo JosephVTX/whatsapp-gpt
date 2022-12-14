41c2108df42649e0c1775ef739356d6bdb855ef1
Joseph VTX <1395609@senati.pe>
2022-12-14 03:04:04 -0500

    style: prettier
    prettier

--- src/connection.ts
+++ src/connection.ts
@@ -1,26 +1,23 @@
-import makeWASocket, { useMultiFileAuthState } from "@adiwajshing/baileys";
+import makeWASocket, { useMultiFileAuthState } from '@adiwajshing/baileys'
 import PINO from 'pino'
 
 const connectWhatsApp = async () => {
+  const { state, saveCreds } = await useMultiFileAuthState('auth_whatsapp')
+  const sock = await makeWASocket({
+    printQRInTerminal: true,
+    auth: state,
+    logger: PINO({ level: 'error' }),
+    syncFullHistory: false
+  })
 
-    const { state, saveCreds } = await useMultiFileAuthState("auth_whatsapp");
-    const sock = await makeWASocket({
+  await sock.ev.on('connection.update', ({ connection }) => {
+    if (connection === 'close') {
+      saveCreds()
+      connectWhatsApp()
+    }
+  })
 
-        printQRInTerminal: true,
-        auth: state,
-        logger: PINO({ level: "error" }),
-        syncFullHistory: false
-    })
-
-    await sock.ev.on("connection.update", ({ connection }) => {
-        if (connection === "close") {
-            saveCreds();
-            connectWhatsApp();
-        }
-    });
-
-    return await sock
-
+  return await sock
 }
 
 export default connectWhatsApp
